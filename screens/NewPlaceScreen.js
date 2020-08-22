bab36ac51fae3362cfc1852efa3341260cb575ca
adclleva <arvin.lleva@yahoo.com>
2020-08-22 15:08:17 -0700

    Got started withe the form for the NewPlaceScreen compoonent and also installing redux redux-thunk and react-redux libraries

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,11 +1,34 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { TextInput, ScrollView } from "react-native-gesture-handler";
+import Colors from "../constants/Colors";
 
 const NewPlaceScreen = () => {
+  const [titleValue, setTitleValue] = useState("");
+
+  const titleChangeHandler = (text) => {
+    // validation can be added here
+    setTitleValue(text);
+  };
+
+  const savePlaceHandler = () => {};
+
   return (
-    <View>
-      <Text>NewPlaceScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          value={titleValue}
+        />
+        <Button
+          title="Save Place"
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -15,4 +38,19 @@ NewPlaceScreen.navigationOptions = {
 
 export default NewPlaceScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
