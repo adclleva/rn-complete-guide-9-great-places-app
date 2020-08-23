e066e8f81d3c2fce6f7c8482b322a02458232110
adclleva <arvin.lleva@yahoo.com>
2020-08-22 20:38:58 -0700

    Able to get the image data from the camera and set the uri to the pickedImage state and had the ImagePicker conditionally render the image or a message

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Image, Button, Alert } from "react-native";
 import Colors from "../constants/Colors";
 import * as ImagePicker from "expo-image-picker";
@@ -6,6 +6,7 @@ import * as Permissions from "expo-permissions";
 
 // renamed to ImgPicker to avoid name clashing from the xpo library
 const ImgPicker = (props) => {
+  const [pickedImage, setPickedImage] = useState();
   /**
    * we need to make sure to enable permissions for the camera and we utilized the
    * expo-permissions library in order to do that
@@ -39,14 +40,33 @@ const ImgPicker = (props) => {
       return;
     }
     // this is an async option that will open up the camera and will return a promise
-    ImagePicker.launchCameraAsync();
+    const image = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    /**
+     * this is an example of what returns from the camera data image  
+    {
+      "cancelled": false,
+      "height": 540,
+      "type": "image",
+      "uri": "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252Frn-complete-guide-9-great-places-app-dc04d009-93dd-4b88-bf90-80cd7c722fb2/ImagePicker/05dadef4-73bd-445a-b6c6-841b6d38e6e3.jpg",
+      "width": 960,
+    }
+     */
+    setPickedImage(image.uri);
   };
 
   return (
     <View style={styles.imagePicker}>
       <View style={styles.imagePreview}>
-        <Text>No image picked yet.</Text>
-        <Image style={styles.image} />
+        {!pickedImage ? (
+          <Text>No image picked yet.</Text>
+        ) : (
+          // we can use the uri to point at local images
+          <Image style={styles.image} source={{ uri: pickedImage }} />
+        )}
       </View>
       <Button
         title="Take Image"
