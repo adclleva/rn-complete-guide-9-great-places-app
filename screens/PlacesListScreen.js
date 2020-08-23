69ecf41eee21bd389784070011d220ad64fe39f5
adclleva <arvin.lleva@yahoo.com>
2020-08-22 17:52:25 -0700

    Able to get the Render a list of Places int he PlacesListScreen by pulling the data from the redux store. Created the PlaceItem where you can press on it to go to the detail screen of the image

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View, Platform } from "react-native";
+import { useSelector } from "react-redux";
+import { StyleSheet, Text, View, Platform, FlatList } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import PlaceItem from "../components/PlaceItem";
 
 const PlacesListScreen = (props) => {
+  const places = useSelector((state) => state.places.places);
+
   return (
-    <View>
-      <Text>PlacesListScreen</Text>
-    </View>
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <PlaceItem
+          image={""}
+          onSelect={() => {
+            props.navigation.navigate("PlaceDetail", {
+              placeTitle: itemData.item.title,
+              placeId: itemData.item.id,
+            });
+          }}
+          address={null}
+          title={itemData.item.title}
+        />
+      )}
+    />
   );
 };
 
