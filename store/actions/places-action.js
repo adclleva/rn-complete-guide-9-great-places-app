eaef69c0291cf5f798a90ff2d7263e2ad52a2357
adclleva <arvin.lleva@yahoo.com>
2020-08-23 17:36:15 -0700

    Added the expo-file-system library in order to move and store the image on the local device within the place-actions store

--- store/actions/places-action.js
+++ store/actions/places-action.js
@@ -1,11 +1,38 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title, image) => {
-  return {
-    type: ADD_PLACE,
-    placeData: {
-      title: title,
-      image: image,
-    },
+  return async (dispatch) => {
+    // image is the temporary file name and will get the file name
+    const fileName = image.split("/").pop();
+
+    // we generate a path and append the file name
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    /**
+     * we will need to move the file of the image and will persist within the app of the device
+     * this is where we will move the file from one place to another with the moveAsync method
+     *  and will return a promise
+     * we do it in a try catch block just in case it fails
+     */
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+    dispatch({
+      type: ADD_PLACE,
+      placeData: {
+        title: title,
+        image: newPath,
+      },
+    });
   };
 };
